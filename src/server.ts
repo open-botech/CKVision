a8e064ce5cac4b7910dc9d4e7d625d02661e055f
Cheskel Twersky <twerskycheskel@gmail.com>
2024-03-17 22:21:08 +0200

    exp

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from 'express'
+import serveStatic from 'serve-static'
+import path from 'path'
+import fs from 'fs'
+import { parse } from 'node-html-parser'
+
+
+const app = express()
+
+
+
+function serveIndex(req: Request, res: Response) {
+  const url = process.env.CONNECTION_URL
+  const user = process.env.CONNECTION_USER
+  const pass = process.env.CONNECTION_PASS
+  const html = fs.readFileSync(path.join('click-cat', 'index.html'), 'utf-8')
+  const root = parse(html.toString())
+  const script = root.querySelector('script')
+  script?.insertAdjacentHTML('beforeend', `
+  localStorage.setItem('previousConnection', JSON.stringify({"previousConnection":{"connectionName":"sonic","connectionUrl":"${url}","username":"${user}","password":"${pass}","params":""}}));
+  localStorage.setItem('connection', JSON.stringify({"connection":{"connectionName":"sonic","connectionUrl":"${url}","username":"${user}","password":"${pass}","params":""}}));
+  `)
+
+  const content = root.toString()
+  res.send(content)
+}
+
+app.get('/',serveIndex)
+
+app.use(serveStatic(path.join( 'click-cat')))
+
+
+app.get('/*',serveIndex)
+
+app.listen(8080)
