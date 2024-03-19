bd762c4471b33cd7a53809ad63a8ddd8255ef70f
Cheskel Twersky <twerskycheskel@gmail.com>
2024-03-19 20:27:50 +0200

    cache

--- src/server.ts
+++ src/server.ts
@@ -1,42 +1,39 @@
-import express, { Request, Response } from 'express'
-import serveStatic from 'serve-static'
-import path from 'path'
-import fs from 'fs'
-import { parse } from 'node-html-parser'
+import express, { Request, Response } from 'express';
+import serveStatic from 'serve-static';
+import path from 'path';
+import fs from 'fs';
+import { parse } from 'node-html-parser';
 
+const app = express();
 
-const app = express()
-
-
-// remove _cat from the url
-app.use((req, res, next) => {
-  if (req.url.includes('_cat')) {
-    req.url = req.url.replace('_cat', '')
-  }
-  next()
-})
-
-function serveIndex(req: Request, res: Response) {
-  const url = process.env.CONNECTION_URL
-  const user = process.env.CONNECTION_USER
-  const pass = process.env.CONNECTION_PASS
-  const html = fs.readFileSync(path.join('click-cat', 'index.html'), 'utf-8')
-  const root = parse(html.toString())
-  const script = root.querySelector('script')
-  script?.insertAdjacentHTML('beforeend', `
-  localStorage.setItem('previousConnection', JSON.stringify({"previousConnection":{"connectionName":"sonic","connectionUrl":"${url}","username":"${user}","password":"${pass}","params":""}}));
-  localStorage.setItem('connection', JSON.stringify({"connection":{"connectionName":"sonic","connectionUrl":"${url}","username":"${user}","password":"${pass}","params":""}}));
-  `)
-
-  const content = root.toString()
-  res.send(content)
+function cacheMiddleware(req: Request, res: Response, next: Function) {
+  res.setHeader('Cache-Control', 'public, max-age=86400');
+  next();
 }
 
-app.get('/',serveIndex)
+function serveIndex(req: Request, res: Response) {
+  const url = process.env.CONNECTION_URL;
+  const user = process.env.CONNECTION_USER;
+  const pass = process.env.CONNECTION_PASS;
+  const html = fs.readFileSync(path.join('click-cat', 'index.html'), 'utf-8');
+  const root = parse(html.toString());
+  const script = root.querySelector('script');
+  script?.insertAdjacentHTML(
+    'beforeend',
+    `
+  localStorage.setItem('previousConnection', JSON.stringify({"previousConnection":{"connectionName":"sonic","connectionUrl":"${url}","username":"${user}","password":"${pass}","params":""}}));
+  localStorage.setItem('connection', JSON.stringify({"connection":{"connectionName":"sonic","connectionUrl":"${url}","username":"${user}","password":"${pass}","params":""}}));
+  `
+  );
 
-app.use(serveStatic(path.join( 'click-cat')))
+  const content = root.toString();
+  res.send(content);
+}
 
+app.get('/', serveIndex);
 
-app.get('/*',serveIndex)
+app.use(cacheMiddleware, serveStatic(path.join('click-cat')));
 
-app.listen(8080)
+app.get('/*', serveIndex);
+
+app.listen(8080);
